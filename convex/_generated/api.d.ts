8d7dfb96f13f161aeb6be91bf31f664244db9d24
kelvinwambua <shwiftykelvin@gmail.com>
2025-11-07 18:44:44 +0300

    About Page and Start Order Work Well

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as admin from "../admin.js";
 import type * as auth from "../auth.js";
 import type * as dashboard from "../dashboard.js";
 import type * as http from "../http.js";
@@ -30,6 +31,7 @@ import type {
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  admin: typeof admin;
   auth: typeof auth;
   dashboard: typeof dashboard;
   http: typeof http;
